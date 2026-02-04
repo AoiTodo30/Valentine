2ae0724f82f73cad1f1aea143b5a61d72a16f950
AoiTodo30 <joeywang0162@gmail.com>
2026-02-04 13:23:52 +0000

    Add Valentine website

---
+++ script.js
@@ -0,0 +1,58 @@
+const yesButton = document.getElementById("yesButton");
+const noButton = document.getElementById("noButton");
+const question = document.getElementById("question");
+const gif = document.getElementById("gif");
+const content = document.getElementById("content");
+
+let noCount = 0;
+let yesButtonSize = 16;
+
+const phrases = [
+  "No",
+  "Are you sure?",
+  "What if I asked really nicely?",
+  "Baby please",
+  "With chocolate on top",
+  "What about a Adana Kebap?",
+  "PLEASE HAYATIM",
+  "But :*(",
+  "I am going to die",
+  "Yep im dead",
+  "ok ur talking to Joey's ghost",
+  "please babe",
+  ":((((",
+  "PRETTY PLEASE",
+  "Estoy muerto",
+  "No :(",
+];
+
+function getNoButtonText() {
+  return phrases[Math.min(noCount, phrases.length - 1)];
+}
+
+yesButton.addEventListener("click", () => {
+  // Zeige die Erfolgsseite
+  content.innerHTML = `
+    <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" style="height: 200px;" />
+    <div style="font-size: 2rem; font-weight: bold; margin-top: 1rem;">WOOOOOO!!! I love you pookie!! ;))</div>
+  `;
+});
+
+noButton.addEventListener("click", () => {
+  noCount += 1;
+  
+  // Update "No" Button Text
+  noButton.textContent = getNoButtonText();
+  
+  // Vergrößere "Yes" Button
+  yesButtonSize = noCount * 20 + 16;
+  yesButton.style.fontSize = yesButtonSize + "px";
+  yesButton.style.padding = (yesButtonSize / 2) + "px " + (yesButtonSize * 1.5) + "px";
+  
+  // Bewege "No" Button zufällig
+  const randomX = Math.floor(Math.random() * 200) - 100;
+  const randomY = Math.floor(Math.random() * 200) - 100;
+  noButton.style.position = "relative";
+  noButton.style.left = randomX + "px";
+  noButton.style.top = randomY + "px";
+});
